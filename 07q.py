2c68f543c9f0266106e072640a30e7aaa3df0fa8
Mariagomesdelima <107954468+Mariagomesdelima@users.noreply.github.com>
2022-06-22 10:41:17 -0300

    Add files via upload

---
+++ 07q.py
@@ -0,0 +1,19 @@
+chomem = 0
+cmulher = 0
+
+for i in range(0, 56):
+  nome = input("escreva seu nome")
+  sexo = str(input('escreva seu sexo: '))
+
+  if sexo == 'masculino' :
+    chomem = chomem + 1 
+    print(nome)
+    print(' você é homem ')
+
+  elif sexo == 'feminino':
+    cmulher = cmulher + 1
+    print(nome)
+    print ('você é mulher')
+
+print('o numero de mulheres é: {}'.format(cmulher))
+print('o numero de homens é: %d' % chomem)
